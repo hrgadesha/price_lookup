ddb252159f709be5fd0c38470c2bfde06d865e84
Hardik Gadesha <hardik@goldsetu.co>
2023-10-30 12:42:26 +0000

    Quotation Price History

--- price_lookup/hooks.py
+++ price_lookup/hooks.py
@@ -29,7 +29,9 @@ app_license = "MIT"
 # page_js = {"page" : "public/js/file.js"}
 
 # include js in doctype views
-# doctype_js = {"doctype" : "public/js/doctype.js"}
+doctype_js = {
+    "Quotation" : "public/js/quotation.js"
+}
 # doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
 # doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
 # doctype_calendar_js = {"doctype" : "public/js/doctype_calendar.js"}
@@ -199,3 +201,8 @@ app_license = "MIT"
 # auth_hooks = [
 #	"price_lookup.auth.validate"
 # ]
+
+fixtures = [
+    {"dt": "Custom Field", "filters": [["module", "=", "Price Lookup"]]},
+    {"dt": "Property Setter", "filters": [["module", "=", "Price Lookup"]]},
+]
