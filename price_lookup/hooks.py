12de19d70eb978869bed9c29cd4759c3b512fba2
Hardik Gadesha <hardik@goldsetu.co>
2023-11-28 06:58:22 +0000

    Historical Data On RFQ and Supplier Quotation

--- price_lookup/hooks.py
+++ price_lookup/hooks.py
@@ -31,7 +31,9 @@ app_license = "MIT"
 # include js in doctype views
 doctype_js = {
     "Quotation" : "public/js/quotation.js",
-    "Sales Order" : "public/js/sales_order.js"
+    "Sales Order" : "public/js/sales_order.js",
+    "Request for Quotation" : "public/js/request_for_quotation.js",
+    "Supplier Quotation" : "public/js/supplier_quotation.js"
 }
 # doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
 # doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
